7b31f40f398474fe70bd9db9c46211ec9eaf2ce2
hong0708 <gaogai2@naver.com>
2020-04-24 01:26:34 +0900

    Add files via upload
    1차 디자인

---
+++ apple1/apple1/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace apple1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sidepnl = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.logopnl = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toppnl = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.sidepnl.SuspendLayout();
+            this.logopnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toppnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sidepnl
+            // 
+            this.sidepnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.sidepnl.Controls.Add(this.button7);
+            this.sidepnl.Controls.Add(this.button6);
+            this.sidepnl.Controls.Add(this.button5);
+            this.sidepnl.Controls.Add(this.button4);
+            this.sidepnl.Controls.Add(this.button3);
+            this.sidepnl.Controls.Add(this.button2);
+            this.sidepnl.Controls.Add(this.button1);
+            this.sidepnl.Controls.Add(this.logopnl);
+            this.sidepnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidepnl.Location = new System.Drawing.Point(0, 0);
+            this.sidepnl.Name = "sidepnl";
+            this.sidepnl.Size = new System.Drawing.Size(238, 621);
+            this.sidepnl.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(0, 492);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(238, 70);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "        button7";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button7.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(0, 422);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(238, 70);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "        button6";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button6.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(0, 352);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(238, 70);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "        button5";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button5.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(0, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 70);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "        button4";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button4.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(0, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 70);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "        button3";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button3.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(0, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 70);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "        button2";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button2.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(0, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "        button1";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button_MouseHover);
+            // 
+            // logopnl
+            // 
+            this.logopnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.logopnl.Controls.Add(this.pictureBox1);
+            this.logopnl.Controls.Add(this.label1);
+            this.logopnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logopnl.Location = new System.Drawing.Point(0, 0);
+            this.logopnl.Name = "logopnl";
+            this.logopnl.Size = new System.Drawing.Size(238, 72);
+            this.logopnl.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(68, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New World";
+            // 
+            // toppnl
+            // 
+            this.toppnl.BackColor = System.Drawing.Color.White;
+            this.toppnl.Controls.Add(this.label4);
+            this.toppnl.Controls.Add(this.label3);
+            this.toppnl.Controls.Add(this.label2);
+            this.toppnl.Controls.Add(this.pictureBox3);
+            this.toppnl.Controls.Add(this.pictureBox2);
+            this.toppnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toppnl.Location = new System.Drawing.Point(238, 0);
+            this.toppnl.Name = "toppnl";
+            this.toppnl.Size = new System.Drawing.Size(849, 72);
+            this.toppnl.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(175, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 30);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Dsah Board";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(783, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(809, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(24, 16);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(103, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1087, 621);
+            this.Controls.Add(this.toppnl);
+            this.Controls.Add(this.sidepnl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.sidepnl.ResumeLayout(false);
+            this.logopnl.ResumeLayout(false);
+            this.logopnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toppnl.ResumeLayout(false);
+            this.toppnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel sidepnl;
+        private System.Windows.Forms.Panel logopnl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel toppnl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ apple1/apple1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apple1
+{
+    public partial class Form1 : Form
+    {
+
+        
+        private Point mousePoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                Location = new Point(this.Left - (mousePoint.X - e.X),
+                    this.Top - (mousePoint.Y - e.Y));
+            }
+        }
+
+        private void button_MouseHover(object sender, EventArgs e)
+        {
+            var buttons = sender as Button;
+
+            if (buttons == button1)
+            {
+                button1.ForeColor = Color.Orange;
+            }
+            else if (buttons == button2)
+            {
+                button2.ForeColor = Color.Orange;
+            }
+            else if (buttons == button3)
+            {
+                button3.ForeColor = Color.Orange;
+            }
+            else if (buttons == button4)
+            {
+                button4.ForeColor = Color.Orange;
+            }
+            else if (buttons == button5)
+            {
+                button5.ForeColor = Color.Orange;
+            }
+            else if (buttons == button6)
+            {
+                button6.ForeColor = Color.Orange;
+            }
+            else if (buttons == button7)
+            {
+                button7.ForeColor = Color.Orange;
+            }
+        }
+
+        private void button_MouseLeave(object sender, EventArgs e)
+        {
+            var buttons = sender as Button;
+            if (buttons == button1)
+            {
+                button1.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button2)
+            {
+                button2.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button3)
+            {
+                button3.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button4)
+            {
+                button4.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button5)
+            {
+                button5.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button6)
+            {
+                button6.ForeColor = Color.Transparent;
+            }
+            else if (buttons == button7)
+            {
+                button7.ForeColor = Color.Transparent;
+            }
+        }
+    }
+}
