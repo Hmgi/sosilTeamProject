597ea204308e631dd163dbf28a035e3f2ab0cda1
kyungbae <48900627+kimkyungbae@users.noreply.github.com>
2020-06-14 16:04:06 +0900

    Add files via upload

---
+++ FileExplorer/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace FileExplorer
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Drv_list;
+            TreeNode root;
+            Drv_list = Environment.GetLogicalDrives();
+
+            foreach(string Drv in Drv_list)
+            {
+                root = trvDir.Nodes.Add(Drv);
+                root.ImageIndex = 2;
+
+                if(trvDir.SelectedNode == null)
+                    trvDir.SelectedNode = root;
+                root.SelectedImageIndex = root.ImageIndex;
+                root.Nodes.Add("");
+            }
+        }
+        public void setPlus(TreeNode node)
+        {
+            string path;
+            DirectoryInfo dir;
+            DirectoryInfo[] di;
+
+            try
+            {
+                path = node.FullPath;
+                dir = new DirectoryInfo(path);
+                di = dir.GetDirectories();
+                if (di.Length > 0)
+                    node.Nodes.Add("");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void OpenFiles()
+        {
+            ListView.SelectedListViewItemCollection siList;
+            siList = lvwFiles.SelectedItems;
+
+            foreach(ListViewItem item in siList)
+            {
+                OpenItem(item);
+            }
+        }
+
+        public void OpenItem(ListViewItem item)
+        {
+            TreeNode node;
+            TreeNode child;
+
+            if(item.Tag.ToString() == "D")
+            {
+                node = trvDir.SelectedNode;
+                node.Expand();
+
+                child = node.FirstNode;
+
+                while(child != null)
+                {
+                    if(child.Text == item.Text)
+                    {
+                        trvDir.SelectedNode = child;
+                        trvDir.Focus();
+                        break;
+                    }
+                    child = child.NextNode;
+                }
+            }
+            else
+            {
+                Process.Start(txtPath.Text + "\\" + item.Text);
+            }
+        }
+
+        private void trvDir_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            string path;
+            DirectoryInfo dir;
+            DirectoryInfo[] di;
+            TreeNode node;
+
+            try
+            {
+                e.Node.Nodes.Clear();
+                path = e.Node.FullPath;
+                dir = new DirectoryInfo(path);
+                di = dir.GetDirectories();
+
+                foreach(DirectoryInfo dirs in di)
+                {
+                    node = e.Node.Nodes.Add(dirs.Name);
+                    setPlus(node);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void trvDir_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            DirectoryInfo di;
+            DirectoryInfo[] diarray;
+            ListViewItem item;
+            FileInfo[] fiarray;
+
+            try 
+            {
+                di = new DirectoryInfo(e.Node.FullPath);
+                txtPath.Text = e.Node.FullPath.Substring(0, 2) + e.Node.FullPath.Substring(3);
+                lvwFiles.Items.Clear();
+
+                diarray = di.GetDirectories();
+                foreach(DirectoryInfo tdis in diarray)
+                {
+                    item = lvwFiles.Items.Add(tdis.Name);
+                    item.SubItems.Add("");
+                    item.SubItems.Add(tdis.LastWriteTime.ToString());
+                    item.ImageIndex = 0;
+                    item.Tag = "D";
+                }
+                fiarray = di.GetFiles();
+                foreach(FileInfo fis in fiarray)
+                {
+                    item = lvwFiles.Items.Add(fis.Name);
+                    item.SubItems.Add(fis.Length.ToString());
+                    item.SubItems.Add(fis.LastWriteTime.ToString());
+                    item.ImageIndex = 1;
+                    item.Tag = "F";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void lvwFiles_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void mnuView_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+
+            mnuDetail.Checked = false;
+            mnuList.Checked = false;
+            mnuSmall.Checked = false;
+            mnuLarge.Checked = false;
+
+            switch(item.Text)
+            {
+                case "자세히":
+                    mnuDetail.Checked = true;
+                    lvwFiles.View = View.Details;
+                    break;
+                case "간단히":
+                    mnuList.Checked = true;
+                    lvwFiles.View = View.List;
+                    break;
+                case "작은 아이콘":
+                    mnuSmall.Checked = true;
+                    lvwFiles.View = View.SmallIcon;
+                    break;
+                case "큰 아이콘":
+                    mnuLarge.Checked = true;
+                    lvwFiles.View = View.LargeIcon;
+                    break;
+            }
+        }
+        public readonly byte[] salt = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+        public const int iterations = 1042;
+
+        public static void EncryptFile(string inputFile, string outputFile, string path)
+        {
+            string currentPath = Environment.CurrentDirectory; // 현재 디렉토리 경로
+
+            string sourceFile = currentPath + @"\" + outputFile; //현재 암호화 파일 경로
+            string DestinationFile = path + @"\" + outputFile; //이동할 암호화 파일 경로
+
+            string password = @"myKey123"; // Your Key Here
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] key = UE.GetBytes(password);
+
+            string cryptFile = outputFile;
+            FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+            RijndaelManaged RMCrypto = new RijndaelManaged();
+
+            CryptoStream cs = new CryptoStream(fsCrypt, RMCrypto.CreateEncryptor(key, key), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            int data;
+            while ((data = fsIn.ReadByte()) != -1)
+                cs.WriteByte((byte)data);
+
+
+            fsIn.Close();
+            cs.Close();
+            fsCrypt.Close();
+
+            System.IO.File.Move(sourceFile, DestinationFile);
+            System.IO.File.Delete(inputFile);
+        }
+        private void 암호화ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = lvwFiles.SelectedItems[0];
+            string Extension = Path.GetExtension(item.Text);
+
+            string inputfile = txtPath.Text + @"\" + item.Text;
+            string path = txtPath.Text;
+            
+            if(Extension == ".hwp")
+            {
+                string encry = "Encrypted.hwp";
+
+                EncryptFile(inputfile, encry, path);
+
+                MessageBox.Show("암호화 완료");
+            }
+            else if(Extension == ".jpg")
+            {
+                string encry = "Encrypted.jpg";
+
+                EncryptFile(inputfile, encry, path);
+
+                MessageBox.Show("암호화 완료");
+            }
+            else if(Extension == ".pdf")
+            {
+                string encry = "Encrypted.pdf";
+
+                EncryptFile(inputfile, encry, path);
+
+                MessageBox.Show("암호화 완료");
+            }
+        }
+    }
+}
